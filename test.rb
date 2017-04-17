ce2014678a81b85afdd0a5d8b66368dfaa71faca
nishinotakuma <takuuma.717@gmail.com>
2017-04-17 12:16:49 +0900

    コメント

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+# problem=1
+
 # sum = 0
 # (1..999).each do |num|
 #   a = num % 3
@@ -7,6 +9,8 @@
 #   end
 # end
 # puts sum
+
+# problem=2
 
 # sum = 0
 # a = 1
@@ -21,10 +25,14 @@
 # end
 # puts sum + 2
 
+# problem=3
+
 # require 'prime'
 #
 # factors = 600851475143.prime_division
 # puts factors.last[0]
+
+# problem=6
 
 # s = 0
 # t = 0
@@ -33,3 +41,13 @@
 #   t += n
 # end
 # p t ** 2 - s
+
+# problem=9
+
+# (1..1000) do |a|
+#   (1..1000) do |b|
+#     (1..1000) do |c|
+#
+#     end
+#   end
+# end
